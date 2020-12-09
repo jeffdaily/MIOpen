ff08aa36362cd73b5d7ac2b99efff65640f5ad1a
Jeff Daily <jeff.daily@amd.com>
2020-12-09 10:54:01 -0800

    force void return type for atomic fadd

--- src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
+++ src/kernels/composable_kernel/include/utility/amd_buffer_addressing.hpp
@@ -139,12 +139,12 @@ __llvm_amdgcn_buffer_store_bf16x4(ushort4_t vdata,
                                   bool slc) __asm("llvm.amdgcn.buffer.store.v4bf16");
 
 #if CK_USE_AMD_BUFFER_ATOMIC_FADD
-#if CK_HIP_VERSION_FLAT >= 3010020405
-// starting ROCm-3.10, the return type becomes float
-__device__ float
-#else
+//#if CK_HIP_VERSION_FLAT >= 3010020405
+//// starting ROCm-3.10, the return type becomes float
+//__device__ float
+//#else
 __device__ void
-#endif
+//#endif
 __llvm_amdgcn_buffer_atomic_add_f32(float vdata,
                                     int32x4_t rsrc,
                                     index_t vindex,
